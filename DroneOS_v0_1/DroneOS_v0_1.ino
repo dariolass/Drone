5cf384c90df7444056ce4849cf0d8c08e9f7d9da
Dario Lass <mail@dariolass.com>
2015-11-06 19:15:34 +0100

    Thrust calculation
    Added simple, linear thrust calculation

--- DroneOS_v0_1/DroneOS_v0_1.ino
+++ DroneOS_v0_1/DroneOS_v0_1.ino
@@ -3,6 +3,7 @@
  */
 
 boolean readyMessageSent = false;
+boolean messageToIMUSent = false;
 
 //--- FLIGHT  CONTROL VARs ----------
 int pitch;        // rotation along axis with rotors 1 & 3
@@ -78,7 +79,13 @@ void loop() {
     Serial1.write("COMM READY\n");
     readyMessageSent = true;
   }
-  while (Serial1.available() > 0) {
+
+  if (millis() >= 2000 && !messageToIMUSent) {
+    Serial2.write('4');
+    messageToIMUSent = true;
+  }
+  
+  while (Serial1.available() > 0) { // reading WIRELESS SERIAL comm
     int integer1 = Serial1.parseInt(); 
     int integer2 = Serial1.parseInt(); 
     int integer3 = Serial1.parseInt(); 
@@ -90,6 +97,24 @@ void loop() {
       throttle = integer4;
     }
   }
+  
+  //'$accelx,accely,accelz,gyrox,gyroy,gyroz,magx,magy,magz#\n\r'
+
+  while (Serial2.available() > 0) { // reading SENSOR SERIAL comm
+    if (Serial2.read() == '$') {
+      accPitch = Serial2.parseInt();
+      accRoll = Serial2.parseInt();
+      accYaw = Serial2.parseInt();
+      gyroPitch = Serial2.parseInt();
+      gyroRoll = Serial2.parseInt();
+      gyroYaw = Serial2.parseInt();
+      magPitch = Serial2.parseInt();
+      magRoll = Serial2.parseInt();
+      magYaw = Serial2.parseInt();
+    }
+  }
+
+  //Serial.print("Motor 1: "); Serial.print(calculateThrustForRotor(1)); Serial.print("\tMotor 3: "); Serial.println(calculateThrustForRotor(3));
 
   motor1.writeMicroseconds(calculateThrustForRotor(1));
   motor2.writeMicroseconds(calculateThrustForRotor(2));
@@ -98,16 +123,22 @@ void loop() {
 }
 
 int calculateThrustForRotor(int motor) {
-  if (motor == 1 || 3) {
-    // affected by ROLL rotation
-    int baseNumber = throttle + roll;
-    int microsToWrite = map(baseNumber, BASE_NUMBER_MIN, BASE_NUMBER_MAX, MICROS_NUMBER_MIN, MICROS_NUMBER_MAX);
-    return microsToWrite;
-  } else {
-    // affected by PITCH rotation
-    int baseNumber = throttle + pitch;
-    int microsToWrite = map(baseNumber, BASE_NUMBER_MIN, BASE_NUMBER_MAX, MICROS_NUMBER_MIN, MICROS_NUMBER_MAX);
-    return microsToWrite;
+  int factor = 5;
+  int baseNumber = 0;
+  switch (motor) {
+    case 1:
+      baseNumber = throttle + roll - (factor*accPitch);
+      break;
+    case 2:
+      baseNumber = throttle + pitch - (factor*accRoll);
+      break;
+    case 3:
+      baseNumber = throttle + roll + (factor*accPitch);
+      break;
+    case 4:
+      baseNumber = throttle + pitch + (factor*accRoll);
+      break;
   }
+  int microsToWrite = map(baseNumber, BASE_NUMBER_MIN, BASE_NUMBER_MAX, MICROS_NUMBER_MIN, MICROS_NUMBER_MAX);
+  return microsToWrite;
 }
-
