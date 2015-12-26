9e0b5cac9e5ce7ce61c2f7a3e0b3978558551635
Dario Lass <mail@dariolass.com>
2015-12-26 02:02:56 +0100

    I²C connection works as proof of concept

--- RazorFirmware/RazorFirmware.ino
+++ RazorFirmware/RazorFirmware.ino
@@ -1,17 +1,51 @@
-int gyroX;
-int gyroY;
-int gyroZ;
+#include <Wire.h>
 
-int accX;
-int accY;
-int accZ;
+unsigned char readI2C(char deviceAddress, char registerAddress);
+void writeI2C(char deviceAddress, char registerAddress, char data);
 
-void setup() {
+void setup()
+{
   Serial.begin(57600);
-  
+  Wire.begin();
+
+  //Sensor setup, read WHO-AM-I
+  char configByte = 25;
+  writeI2C(0x68, 0x16, 25);
+  writeI2C(0x68, 0x15, 4);
 }
 
-void loop() {
-  
+void loop()
+{
+  int gyroX = 0;
+  gyroX = readI2C(0x68, 0x1D)<<8;
+  gyroX |= readI2C(0x68, 0x1E);
+  Serial.println(gyroX, DEC);
 
+  delay(5);
+}
+
+unsigned char readI2C(char deviceAddress, char registerAddress)
+{
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(registerAddress);
+  Wire.endTransmission();
+
+  Wire.beginTransmission(deviceAddress);
+  Wire.requestFrom(deviceAddress, 1);
+
+  unsigned char data = 0;
+  if (Wire.available()) {
+    data = Wire.read();
+  }
+
+  Wire.endTransmission();
+  return data;
+}
+
+void writeI2C(char deviceAddress, char registerAddress, char data)
+{
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(registerAddress);
+  Wire.write(data);
+  Wire.endTransmission();
 }
